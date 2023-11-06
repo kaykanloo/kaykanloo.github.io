26049aac0594c38e6ca3bc5b526bdd84e21054ea
Kaykanloo <kaykanloo@gmail.com>
2023-11-05 20:56:35 -0500

    Svelte SSG

--- svelte.config.js
+++ svelte.config.js
@@ -7,8 +7,8 @@ const config = {
 	extensions: ['.svelte'],
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
-	preprocess: [ vitePreprocess()],
-	
+	preprocess: [vitePreprocess()],
+
 	vitePlugin: {
 		inspector: true,
 	},
@@ -16,7 +16,15 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter({
+			// default options are shown. On some platforms
+			// these options are set automatically — see below
+			pages: 'build',
+			assets: 'build',
+			fallback: undefined,
+			precompress: false,
+			strict: true
+		})
 	}
 };
 export default config;
