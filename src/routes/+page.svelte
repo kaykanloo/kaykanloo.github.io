b5d046f4dd720765224a16f14e7125f2154d7331
Kaykanloo <kaykanloo@gmail.com>
2023-11-05 20:02:37 -0500

    Site Maintenance

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,10 @@
 <!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
 
 <div class="container h-full mx-auto flex justify-center items-center">
-	<div class="space-y-10 text-center flex flex-col items-center">
-		<h2 class="h2">Welcome to Kaykanloo.</h2>
-	</div>
+  <div class="space-y-10 text-center flex flex-col items-center">
+    <h2 class="h2">Site Maintenance</h2>
+    <p>We are currently working on the site. Please check back later.</p>
+  </div>
 </div>
 
 <style lang="postcss">
